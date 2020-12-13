7c289763e002bd59fcb961fb3b543d2e417f2689
mateuszstraszewski <strachoo@gmail.com>
2020-12-13 11:59:41 +0100

    Exercise 1; 2_sda solved

---
+++ src/main/java/onlineEx_2sda/DateConverter.java
@@ -0,0 +1,27 @@
+package onlineEx_2sda;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DateConverter {
+    private Map<Integer,String> dayName;
+
+    public DateConverter() {
+        this.dayName = new HashMap<>();
+        initDays();
+    }
+
+    private void initDays() {
+        this.dayName.put(1, "Monday");
+        this.dayName.put(2, "Tuesday");
+        this.dayName.put(3, "Wednesday");
+        this.dayName.put(4, "Thursday");
+        this.dayName.put(5, "Friday");
+        this.dayName.put(6, "Saturday");
+        this.dayName.put(7, "Sunday");
+    }
+
+    public void getDayName(int day) {
+        System.out.println("Day name is " + this.dayName.get(day));
+    }
+}
---
+++ src/main/java/onlineEx_2sda/Main.java
@@ -0,0 +1,8 @@
+package onlineEx_2sda;
+
+public class Main {
+    public static void main(String[] args) {
+        DateConverter converter = new DateConverter();
+        converter.getDayName(4);
+    }
+}
